4dbd2bdf122a68929a6792304a27fcb3786663f4
xlearns <2447684095@qq.com>
2022-11-01 15:01:24 +0800

    feat: add lsof close port for ng.config.ts

--- shell-build-nginx/packages/build/Ci.ts
+++ shell-build-nginx/packages/build/Ci.ts
@@ -4,6 +4,7 @@ import { log } from "./utils";
 const { mv, chmod, exec } = shell;
 export default (config) => {
   const { input, output, command } = config;
+  exec(`rm -rf ${output}`)
   mv(input, output);
   log.success("move finish");
   chmod(777, output);
--- shell-build-nginx/packages/config/ng.config.ts
+++ shell-build-nginx/packages/config/ng.config.ts
@@ -1,14 +1,14 @@
 export default {
-  port: 8848,
-  output: "/test",
-  input: "dist",
-  proxy: [["/api", "http://localhost"]],
-  command: [
-    "systemctl stop firewalld.service",
-    "lsof -i :var(port)|grep -v 'PID'|awk '{print 'kill -9',$2}'|sh",
-    "iptables -I INPUT -p tcp --dport var(port) -j ACCEPT",
-    "systemctl start nginx.service",
-    "systemctl reload nginx.service",
-    "netstat -nap | grep var(port)",
-  ],
+	port: 8848,
+	output: "/test",
+	input: "dist",
+	proxy: [["/api", "http://localhost"]],
+	command: [
+		"systemctl stop firewalld.service",
+		`lsof -i :var(port)|grep -v 'PID'|awk '{print "kill -9",$2}' |sh`,
+		"iptables -I INPUT -p tcp --dport var(port) -j ACCEPT",
+		"systemctl start nginx.service",
+		"systemctl reload nginx.service",
+		"netstat -nap | grep var(port)",
+	],
 };
